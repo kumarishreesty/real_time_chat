8052babc152ed9ef1db420fbfbb3be8dca9bcde5
kumarishreesty <shreestykri14200@gmail.com>
2024-11-22 21:12:08 +0530

    project done

--- app/Livewire/ChatComponent.php
+++ app/Livewire/ChatComponent.php
@@ -2,13 +2,23 @@
 
 namespace App\Livewire;
 
+use App\Models\Message;
+use App\Events\MessageSentEvent;
 use App\Models\User;
+use Livewire\Attributes\On;
 use Livewire\Component;
 
 class ChatComponent extends Component
 {
 
     public $user;
+    public $sender_id;
+    public $receiver_id;
+    public $message;
+
+    public $messages = [];
+
+
     public function render()
     {
         return view('livewire.chat-component');
@@ -17,6 +27,64 @@ class ChatComponent extends Component
     }
 
     public function mount($user_id){
+
+        $this->sender_id = auth()->user()->id;
+        $this->receiver_id = $user_id;
+
+        $messages = Message::where(function($query){
+            $query->where('sender_id', $this->sender_id)
+                  ->where('receiver_id', $this->receiver_id);
+        })->orWhere(function($query){
+            $query->where('sender_id', $this->receiver_id)
+                  ->where('receiver_id', $this->sender_id);
+        })
+        ->with('sender:id,name', 'receiver:id,name')
+        ->get();
+
+
+        foreach($messages as $message){
+            $this->appendChatMessage($message);
+        }
+        
        $this->user = User::whereId($user_id)->first();
     }
+
+    public function sendMessage(){
+
+        // dd($this->message);
+        $chatMessage = new Message();
+        $chatMessage->sender_id = $this->sender_id;
+        $chatMessage->receiver_id = $this->receiver_id;
+        $chatMessage->message = $this->message;
+        $chatMessage->save();
+
+        $this->appendChatMessage($chatMessage);
+
+        broadcast(new MessageSentEvent($chatMessage))->toOthers();
+
+        $this->message = '';
+    }
+
+    #[On('echo-private:chat-channel.{sender_id},MessageSentEvent')]
+    public function listenForMessage($event){
+        $chatMessage = Message::whereId($event['message']['id'])
+            ->with('sender:id,name', 'receiver:id,name')
+            ->first();
+
+        $this->appendChatMessage($chatMessage);
+    }
+
+
+
+    public function appendChatMessage($message){
+        $this->messages[] = [
+            'id' => $message->id,
+            'message' => $message->message,
+            'sender' => $message->sender->name,
+            'receiver' => $message->receiver->name
+        ];
+    }
+
+
+        
 }
